d7baac282e206c7b38829465ccd58bb40443ffaa
Palle Klewitz <palle.klewitz@tum.de>
2017-12-18 17:47:22 +0100

    Initial Commit

---
+++ Graphite/Graphite.h
@@ -0,0 +1,19 @@
+//
+//  Graphite.h
+//  Graphite
+//
+//  Created by Palle Klewitz on 18.12.17.
+//  Copyright © 2017 Palle Klewitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graphite.
+FOUNDATION_EXPORT double GraphiteVersionNumber;
+
+//! Project version string for Graphite.
+FOUNDATION_EXPORT const unsigned char GraphiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graphite/PublicHeader.h>
+
+
